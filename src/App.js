a6735813acab02adc77dd1c0765e542fc60c8eab
GHOSTofTIME <ilya2005kovt@mail.ru>
2023-12-04 13:28:02 +0300

    первая версия

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './componetns/Header';
+import Navbar from './componetns/NavBar';
+import Profile from "./componetns/Profile";
+import Footer from "./componetns/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      HEllo
-    </div>
-  );
+    <div className='app-wrapper'>
+      <Header />
+      <Navbar />
+      <Profile />
+      <Footer />
+    </div>);
 }
 
 export default App;
